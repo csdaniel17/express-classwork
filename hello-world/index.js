9ac43ab3755e710ce278d4be8a25355382429d3e
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-24 10:11:58 -0500

    updated using express

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,18 +1,38 @@
+/*
+long version
+*/
+// 
+// var express = require('express');
+// var http = require('http');
+//
+// var hostname = 'localhost';
+// var port = 3000;
+//
+// var app = express();
+//
+// app.use(function(reg, res, next) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('<html><body><h1>Hello World</h1></body></html>');
+// });
+//
+// var server = http.createServer(app);
+//
+// server.listen(port, hostname, function() {
+//   console.log('server is running at http://' + hostname + ':' + port);
+// });
+
+
+/*
+express simplification
+*/
+
 var express = require('express');
-var http = require('http');
-
-var hostname = 'localhost';
-var port = 3000;
-
 var app = express();
 
-app.use(function(reg, res, next) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body><h1>Hello World</h1></body></html>');
+app.get('/', function(req, res) {
+  res.send('Hello World');
 });
 
-var server = http.createServer(app);
-
-server.listen(port, hostname, function() {
-  console.log('server is running at http://' + hostname + ':' + port);
+app.listen(3000, function() {
+  console.log('app is listening on port 3000');
 });
